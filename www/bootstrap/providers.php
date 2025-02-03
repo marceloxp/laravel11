fc629f40aa1530c3092b7a544f80a77d5b7d771d
Marcelo XP <marceloxp@gmail.com>
2025-02-03 08:06:50 -0300

    - Added initial Filament.

--- www/bootstrap/providers.php
+++ www/bootstrap/providers.php
@@ -3,5 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\DataSiteServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\MetaSocialServiceProvider::class,
 ];
